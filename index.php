cf762095772b82ec3564fbaa0baf502745da92fe
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2023-12-12 14:37:41 +0100

    Iterator

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Iterator\StepIterator;
 use App\User\Admin;
 use App\User\Interface\AuthInterface;
 
@@ -7,13 +8,13 @@ require_once __DIR__.'/vendor/autoload.php';
 
 $m1 = Admin::create('Bob', 'admin1234', 35, false);
 
-function run(AuthInterface $user, array $argv): never
+function auth(array $argv, AuthInterface $user): never
 {
-    $login = $argv[1];
-    $password = $argv[2];
+    $login = $argv[2];
+    $password = $argv[3];
 
     try {
-        $user->auth($argv[1], $argv[2]);
+        $user->auth($argv[2], $argv[3]);
         echo $user;
     } catch (\Exception) {
         echo "Authentication failed.\n";
@@ -22,4 +23,20 @@ function run(AuthInterface $user, array $argv): never
     exit(0);
 }
 
-run($m1, $argv);
+function step(array $argv): never
+{
+    $total = $argv[2];
+    $step = $argv[3];
+
+    $iterator = new StepIterator(range(0, $total), $step);
+
+    foreach ($iterator->toGenerator() as $key => $value) {
+        echo sprintf("%d => %d\n", $key, $value);
+    }
+
+    exit(0);
+}
+
+if (function_exists($argv[1])) {
+    $argv[1]($argv, $m1);
+}
